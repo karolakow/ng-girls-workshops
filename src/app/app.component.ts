9439f74e3a054d0afd3a94bfefa443366fa8e052
ist1105122 <karolina.kinga.kowalczyk@tecnico.ulisboa.pt>
2024-11-02 10:44:43 +0100

    9 the todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,38 @@ import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.
   standalone: true,
   imports: [RouterOutlet, InputButtonUnitComponent],
   //templateUrl: './app.component.html',
-  template: ` <h1>Welcome to {{ title }}!</h1>
-    <app-input-button-unit></app-input-button-unit>`,
+  template: `
+    <h1>Welcome to {{ title }}!</h1>
+    <app-input-button-unit></app-input-button-unit>
+    <ul>
+      @for (todoItem of todoList; track todoItem.title) {
+      <li>
+        {{ todoItem.title }}
+      </li>
+      }
+    </ul>
+    @if(userLoggedIn) {
+    <h1>Welcome!</h1>
+    <h2>You are logged in</h2>
+    } @else {
+    <h3>Please log in</h3>
+    }
+  `,
+
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'todo-list';
+  userLoggedIn = false;
+
+  todoList = [
+    { title: 'install NodeJS' },
+    { title: 'install Angular CLI' },
+    { title: 'create new app' },
+    { title: 'serve app' },
+    { title: 'develop app' },
+    { title: 'deploy app' },
+  ];
 
   constructor() {
     this.changeTitle('My First Angular App');
