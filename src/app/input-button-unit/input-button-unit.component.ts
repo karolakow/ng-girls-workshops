3eed5ecc3b51bffc95085060544ed07fcfe86d8d
ist1105122 <karolina.kinga.kowalczyk@tecnico.ulisboa.pt>
2024-11-02 10:37:02 +0100

    8 element ref #

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -1,22 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
   standalone: true,
   imports: [],
   template: `
-    <p>input-button-unit works!</p>
-    The item title is: {{ title }}
-    <input [value]="title" (keyup.enter)="changeTitle(getInputValue($event))" />
+    <p>input-button-unit works! The item title is: {{ title }}</p>
+    <input
+      #inputElementRef
+      [value]="title"
+      (keyup.enter)="changeTitle(getInputValue($event))"
+    />
     <!-- <p>Generated title</p>
     <input [value]="generateTitle()" />
     <p>{{ generateTitle() }}</p> -->
     <!-- <input id="my-input" /> -->
-    <button (click)="changeTitle('Button Clicked!')">Save</button>
+    <button (click)="changeTitle(inputElementRef.value)">Save</button>
   `,
   styleUrl: './input-button-unit.component.scss',
 })
-export class InputButtonUnitComponent {
+export class InputButtonUnitComponent implements OnInit {
   title = 'Hello world';
 
   constructor() {
@@ -24,7 +27,9 @@ export class InputButtonUnitComponent {
     //   this.title = 'This is not the title you are looking for';
     // }, 3000);
   }
+  ngOnInit(): void {}
   // ngAfterViewInit(): void {
+  // // bad practise
   //   let inputElement = document.getElementById('my-input') as HTMLInputElement;
   //   console.log(inputElement);
   //   inputElement && (inputElement.value = this.title);
